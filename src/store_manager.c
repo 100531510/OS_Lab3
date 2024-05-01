30a7c6438fc3cd28f6982d0e3dd24652ddc7ce6d
KurtUC3M <100531523@alumnos.uc3m.es>
2024-05-01 12:50:49 +0200

    concurrency works? but like not really

--- src/store_manager.c
+++ src/store_manager.c
@@ -80,22 +80,46 @@ void *consumer(void *args)
   associated_data *data = (associated_data *)args;
   struct element curOp;
 
+  while (true)
+  {
+
+    pthread_mutex_lock(&prod_ctrl.lock);
+    printf("Consumer waiting: active_producers=%d, buffer_empty=%d\n", prod_ctrl.active_producers, queue_empty(data->buffer));
+
+    while (prod_ctrl.active_producers > 0 && queue_empty(data->buffer))
+    {
+      pthread_cond_wait(&prod_ctrl.alldone, &prod_ctrl.lock);
+    }
+    if (prod_ctrl.active_producers == 0 && queue_empty(data->buffer))
+    {
+      pthread_mutex_unlock(&prod_ctrl.lock);
+      break;
+    }
+    pthread_mutex_unlock(&prod_ctrl.lock);
+    // once there is an element we can extract it
+    curOp = *(queue_get(data->buffer));
+
+    printf("Consuming: Product ID=%d, Operation=%d, Units=%d\n", curOp.product_id, curOp.op, curOp.units);
+
+    // compute profit and stock
+    if (curOp.op == 1)
+    {
+      // PURCHASE
+      *data->profits -= (purchaseCosts[curOp.product_id - 1] * curOp.units);
+      data->product_stock[curOp.product_id - 1] += curOp.units;
+    }
+    else
+    {
+      // SALE
+      *data->profits += (salesPrices[curOp.product_id - 1] * curOp.units);
+      data->product_stock[curOp.product_id - 1] -= curOp.units;
+    }
+  }
+
   /*
-    while (true)
+    for (int i = 0; i < data->num_operations; ++i)
     {
 
-      pthread_mutex_lock(&prod_ctrl.lock);
-
-      while (prod_ctrl.active_producers > 0 && queue_empty(data->buffer))
-      {
-        pthread_cond_wait(&prod_ctrl.alldone, &prod_ctrl.lock);
-      }
-      if (prod_ctrl.active_producers == 0 && queue_empty(data->buffer))
-      {
-        pthread_mutex_unlock(&prod_ctrl.lock);
-        break;
-      }
-      pthread_mutex_unlock(&prod_ctrl.lock);
       // once there is an element we can extract it
       curOp = *(queue_get(data->buffer));
 
@@ -114,28 +138,7 @@ void *consumer(void *args)
       }
     }
   */
-
-  for (int i = 0; i < data->num_operations; ++i)
-  {
-
-    // once there is an element we can extract it
-    curOp = *(queue_get(data->buffer));
-
-    // compute profit and stock
-    if (curOp.op == 1)
-    {
-      // PURCHASE
-      *data->profits -= (purchaseCosts[curOp.product_id - 1] * curOp.units);
-      data->product_stock[curOp.product_id - 1] += curOp.units;
-    }
-    else
-    {
-      // SALE
-      *data->profits += (salesPrices[curOp.product_id - 1] * curOp.units);
-      data->product_stock[curOp.product_id - 1] -= curOp.units;
-    }
-  }
-
+  printf("Consumer exiting\n");
   pthread_exit(0);
 }
 
